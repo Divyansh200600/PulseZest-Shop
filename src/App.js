e785a16da67e8ed99e3945aed819f56bf87c9976
Divyansh Chauhan <divyansh20060@gmail.com>
2024-07-15 18:37:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import DbPage from './pages/DashBoard/dbPage';
+import LoginPage from './pages/Login/loginPage'; 
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello Admin</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DbPage />} />
+          <Route path="/" element={<h1>Hello Admin</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
